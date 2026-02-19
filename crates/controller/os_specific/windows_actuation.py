e7579a90dd308f54c028d373b03d494f6ef5431d
immortalshadow007 <kartikswamy2000@gmail.com>
2026-02-19 15:06:14 +0530

    fix: Update keyboard command processing to replace '^' with '{Asc 94}' for correct character representation

--- crates/controller/os_specific/windows_actuation.py
+++ crates/controller/os_specific/windows_actuation.py
@@ -290,7 +290,7 @@ class WindowsActuation:
                 kb_content = self._convert_modifiers_to_explicit(kb_content)
                 echo_payload = f'press {kb_content}'
             else:
-                kb_content = kb_content.replace('^', '{^^}')
+                kb_content = kb_content.replace('^', '{Asc 94}')
                 echo_payload = f'type {kb_content}'
 
             shell_cmd = f'cmd /c echo {echo_payload} > C:\\keyboard_cmd.txt'
@@ -392,7 +392,7 @@ class WindowsActuation:
                             kb_content = self._convert_modifiers_to_explicit(kb_content)
                             echo_payload = f'press {kb_content}'
                         else:
-                            kb_content = kb_content.replace('^', '{^^}')
+                            kb_content = kb_content.replace('^', '{Asc 94}')
                             echo_payload = f'type {kb_content}'
                         shell_cmd = f'cmd /c echo {echo_payload} > C:\\keyboard_cmd.txt'
                     else:
